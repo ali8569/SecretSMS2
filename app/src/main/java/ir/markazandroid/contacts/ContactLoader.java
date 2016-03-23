d367c6a89b17d0bdc24c4aeaf933f8a192e7148c
ali8569 <wehatexboc360@icloud.com>
2016-03-23 13:19:02 +0430

    2nd change

--- app/src/main/java/ir/markazandroid/contacts/ContactLoader.java
+++ app/src/main/java/ir/markazandroid/contacts/ContactLoader.java
@@ -1,7 +1,16 @@
 package ir.markazandroid.contacts;
 
+import android.content.Context;
+
 /**
  * Created by Ali on 23/03/2016.
  */
 public class ContactLoader {
+    private Context context;
+
+    public ContactLoader(Context context) {
+        this.context = context;
+    }
+
+
 }
